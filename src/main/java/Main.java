e0d325fce5e6bc87b978ce1089692116e5d9a36a
liriy08 <liri.yefet1@gmail.com>
2025-02-10 16:47:07 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,40 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
-  }
-}
+public static Node<Integer> q15(Node<Integer> a)
+	{
+		Node<Integer> head = a;
+		if (head == null) return null;
+
+		        Node<Integer> b = head; 
+		int maxL = 1;      
+	 	Node<Integer> currentS = head; 
+		int currentL= 1;   
+		Node<Integer> current = head.getNext();
+
+		while (current != null) 
+		{
+		      if (current.getValue() == currentS.getValue()) 
+		      {
+		          currentL++;
+		            } 
+		      else 
+		      {
+		           if (currentL > maxL) 
+		             {
+		                 maxL = currentL;
+		                 b = currentS;
+		             }
+		               
+		              currentS = current;
+		              currentL = 1;
+		     }
+		            current = current.getNext();
+		}
+
+		if (currentL > maxL) 
+		        {
+		            b = currentS;
+		        }
+
+		        return b;
+		    }
+	
+
